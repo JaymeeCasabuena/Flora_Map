8465d94922f64fa28472df76ca7dd45650326308
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2025-01-29 23:55:52 +1000

    Task: Updated dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -39,11 +39,11 @@ const Dashboard = () => {
           <LoadingSpinner />
         </div>
       ) : (
-        <main className="flex flex-row gap-7 w-full py-24">
+        <main className="flex flex-row gap-7 w-full">
           <div className="map w-2/3 ms-5">
             <CustomMap />
           </div>
-          <div className="details rounded-2xl w-1/3 me-5">
+          <div className="details rounded-2xl w-1/3 mt-24 me-5">
             <AddMarkerForm />
           </div>
         </main>
